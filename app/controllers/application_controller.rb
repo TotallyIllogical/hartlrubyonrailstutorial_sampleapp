3802b45b8a9dbaf3375946cf4d87ced64280d594
eiaskog <emma.eia.skog@gmail.com>
2017-12-11 13:18:57 +0100

    Finished static pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,5 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   def hello
     render html: "Hello! This is the app called Sample App from Michael Hartl Runy on Rails Tutorial"
+  end
 end
