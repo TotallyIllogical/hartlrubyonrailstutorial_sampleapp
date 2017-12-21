60afb8f8b9c353b5067cf2e353640a841ccaa2cb
eiaskog <emma.eia.skog@gmail.com>
2017-12-21 11:04:25 +0100

    adjusted gravatar function to fetch profile images for my test users from assets instead of gravatar

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,13 +2,18 @@ module UsersHelper
 
   # Used at app/views/users/show.html.erb
   def gravatar_for(user, options = { size: 80})
-    # Creates an id based in the email
-    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    size = options[:size]
-    # Get the image based on the generated id (if it exists)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
-    # Creates image tag
-    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+    if user.email.include? '@stargate.gov'
+      img_name = user.email.split('@').first
+      image_tag(img_name + '.jpg', alt:user.name, width: options[:size])
+    else
+      # Creates an id based in the email
+      gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+      size = options[:size]
+      # Get the image based on the generated id (if it exists)
+      gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+      # Creates image tag
+      image_tag(gravatar_url, alt: user.name, class: "gravatar")
+    end
   end
 
 end
